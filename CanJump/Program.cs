b3d466cb7014ac8fbcb4290cdd09edef8ede8fa1
shaqablanca <a.sameduz@gmail.com>
2023-05-31 18:16:08 +0200

    Array and Hashing Exercise

--- CanJump/Program.cs
+++ CanJump/Program.cs
@@ -1,38 +1,32 @@
-﻿
-using System;
+﻿using System;
 
-namespace CanJump{ 
+namespace CanJump
+{
 
-    class Program {
-
-        
-
-        public static void Main() {
-
-            int[] nums = { 6, 0, 0, 1, 4};
-       
+    class Program
+    {        
+        public static void Main()
+        {
+            int[] nums = { 6, 0, 0, 1, 4 };
             Console.WriteLine(CanJump(nums));
-            
         }
-    
+        public static bool CanJump(int[] arr)
+        {
 
-
-	    public  static bool CanJump(int[] arr) {
-	   
             int len = arr.Length;
-   
-            if (len == 0) return true; 
-	        
-	        int ind = 0;
-            for ( int i = 0; i < (len - 1) && ind>=i; i++) {
-                if (ind < i + arr[i]) {
-                    ind = i + arr[i];
-		        }
-                if (ind >= (len - 1)) return true;
-	        }
+            if (len == 0) return true;
+            int indx = 0;
+            for (int i = 0; i < (len - 1) && indx >= i; i++)
+            {
+                if (indx < i + arr[i])
+                {
+                    indx = i + arr[i];
+                }
+                if (indx >= (len - 1)) return true;
+            }
             return false;
-           
-	}
+
+        }
     }
 
 }
